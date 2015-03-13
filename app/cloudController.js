53be71966e77b1557a3bbe02737d008ee7ef7a97
Andrea Terzani <andrea.terzani@gmail.com>
2015-03-13 08:35:27 +0100

    FB share info

--- app/cloudController.js
+++ app/cloudController.js
@@ -2,32 +2,17 @@
  * Created by andrea.terzani on 12/03/2015.
  */
 var options ={
-  gridSize: Math.round(16 * 1900/ 1024),
+  gridSize: Math.round(16 * $('#canvas').width() / 1024),
   weightFactor: function (size) {
-    return Math.pow(size, 2.3) * 1900 / 1024;
+    return Math.pow(size, 2.0) * $('#canvas').width() / 1024;
   },
   fontFamily: 'Times, serif',
   color: function (word, weight) {
-    return '#ee6e73';
+    return (weight === 12) ? '#f02222' : '#c09292';
   },
-  rotateRatio: 0.5,
-  backgroundColor: '#ffffff',
-  minSize:2
+  rotateRatio: 0.3,
+  backgroundColor: '#ffffff'
 }
-
-
- function resize_canvas(){
-            canvas = document.getElementById("canvas");
-            if (canvas.width  < window.innerWidth)
-            {
-                canvas.width  = window.innerWidth;
-            }
-
-            if (canvas.height < window.innerHeight)
-            {
-                canvas.height = window.innerHeight;
-            }
-        }
 
 
 app.controller('cloudController',function($scope,Word,$timeout){
@@ -42,7 +27,6 @@ app.controller('cloudController',function($scope,Word,$timeout){
                 $scope.loading=false;
 
             }else{
-            	resize_canvas();
             	
             	var list =[];
 				$scope.words.forEach(function(entry) {
